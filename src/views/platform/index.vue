718d1797c569f5b29bf8487aaef5e1aea0e04d1e
zhifeiji <895807497@qq.com>
2023-06-23 14:13:50 +0800

    fix:platform info

--- src/views/platform/index.vue
+++ src/views/platform/index.vue
@@ -1,144 +1,111 @@
 <template>
   <div class="app-container">
-    <div class="filter-container" style="margin-bottom: 10px;">
-      <el-input
-        v-model="listQuery.name"
-        placeholder="姓名/手机号码"
-        style="width: 200px;margin-left: 5px;"
-        class="filter-item"
-        @keyup.enter.native="handleFilter"
-      />
 
-      <el-date-picker
-        v-model="listQuery.start_time"
-        value-format="timestamp"
-        style="margin-left: 10px;width:150px;"
-        placeholder="最近联系时间段"
-      />
+    <div :key="key" style="margin-top:30px;">
 
-      <el-date-picker
-        v-model="listQuery.end_time"
-        value-format="timestamp"
-        style="margin-left: 10px;width: 150px;"
-        placeholder="最近联系时间段"
-      />
-      <el-button
-        class="filter-item"
-        style="margin-left: 10px;"
-        type="primary"
-        icon="el-icon-search"
-        @click="handleFilter"
-      >
-        搜索
-      </el-button>
+      <div>
+        <span class="permission-alert">
+          商户余额
+          <el-tag class="permission-tag" size="small">{{ detail.business_amount }}</el-tag>
+        </span>
+      </div>
+      <div>
+        <span class="permission-alert">
+          商户编号
+          <el-tag class="permission-tag" size="small">{{ detail.platform_sn }}</el-tag>
+        </span>
+      </div>
+      <div>
+        <span class="permission-alert">
+          名称
+          <el-tag class="permission-tag" size="small">{{ detail.name }}</el-tag>
+        </span>
+      </div>
+      <div>
+        <span class="permission-alert">
+          账号
+          <el-tag class="permission-tag" size="small">{{ detail.account }}</el-tag>
+        </span>
+      </div>
+      <div>
+        <span class="permission-alert">
+          密钥
+          <el-tag class="permission-tag" size="small">{{ detail.private_key }}</el-tag>
+        </span>
+      </div>
+      <div>
+        <span class="permission-alert">
+          白名单
+          <el-tag class="permission-tag" size="small">{{ detail.whitelist }}</el-tag>
+        </span>
+      </div>
 
-    </div>
+      <div>
+        <span class="permission-alert">
+          银行卡代收费率
+          <el-tag class="permission-tag" size="small">{{ detail.bank_collect_val }}/万</el-tag>
+        </span>
+      </div>
 
-    <div>
-      <el-row :gutter="20">
+      <div>
+        <span class="permission-alert">
+          微信代收费率
+          <el-tag class="permission-tag" size="small">{{ detail.wx_collect_val }}/万</el-tag>
+        </span>
+      </div>
 
-        <el-col ref="class_customer_list" class="test" :span="6" :xs="24" style="width:100%;">
+      <div>
+        <span class="permission-alert">
+          支付宝代收费率
+          <el-tag class="permission-tag" size="small">{{ detail.ali_collect_val }}/万</el-tag>
+        </span>
+      </div>
 
-          <el-table
-            v-loading="listLoading"
-            :data="list"
-            row-key="id"
-            border
-            fit
-            highlight-current-row
-            style="margin-top: 5px"
-          >
+      <div>
+        <span class="permission-alert">
+          银行卡代付费率
+          <el-tag class="permission-tag" size="small">{{ detail.bank_out_free }}/万</el-tag>
+        </span>
+      </div>
 
-            <el-table-column label="订单编号" prop="order_no" width="200px" align="center"/>
-            <el-table-column label="订单金额" prop="order_amount" width="150px" align="center"/>
-            <el-table-column label="创建时间" prop="create_time" align="center" width="180px"/>
-            <el-table-column label="状态" prop="status" align="center" width="100px">
-              <template slot-scope="{row}">
-                <el-tag>{{ row.status | statusFilter }}</el-tag>
-              </template>
-            </el-table-column>
-            <el-table-column label="收款名称" prop="pay_name" align="center" width="150px"/>
-            <el-table-column label="收款账户" prop="pay_account" align="center" width="200px"/>
-            <el-table-column label="收款备注" prop="pay_remark" align="center" width="200px"/>
+      <div>
+        <span class="permission-alert">
+          微信代付费率
+          <el-tag class="permission-tag" size="small">{{ detail.wx_out_free }}/万</el-tag>
+        </span>
+      </div>
 
-          </el-table>
+      <div>
+        <span class="permission-alert">
+          支付宝代付费率
+          <el-tag class="permission-tag" size="small">{{ detail.ali_out_free }}/万</el-tag>
+        </span>
+      </div>
 
-          <pagination
-            v-show="total>0"
-            :total="total"
-            :page.sync="listQuery.page"
-            :limit.sync="listQuery.limit"
-            @pagination="getList"
-          />
-
-        </el-col>
-
-      </el-row>
     </div>
 
   </div>
-
 </template>
+
 
 <script>
 import Pagination from '@/components/Pagination'
 import {MessageBox} from 'element-ui'
 
 import {
-  getChangeOrderList
-} from '@/api/out-order'
+  getInfo
+} from '@/api/business'
 
-const status = [
-  {id: '1', name: '待接单'},
-  {id: '2', name: '待付款'},
-  {id: '3', name: '待确认'},
-  {id: '4', name: '已确认'},
-  {id: '5', name: '已取消'}
-]
-
-const typeStatus = [
-  {id: '1', name: '充值'},
-  {id: '2', name: '代付'},
-  {id: '3', name: '商户提现'},
-  {id: '4', name: '用户提现'}
-]
-
-const statusFilterKeyValue = status.reduce((acc, cur) => {
-  acc[cur.id] = cur.name
-  return acc
-}, {})
-
-const typeFilterKeyValue = typeStatus.reduce((acc, cur) => {
-  acc[cur.id] = cur.name
-  return acc
-}, {})
 
 export default {
   name: 'OutOrder',
   // eslint-disable-next-line vue/no-unused-components
   components: {Pagination},
-  filters: {
-    statusFilter(key) {
-      return statusFilterKeyValue[key]
-    },
-    typeFilter(key) {
-      return typeFilterKeyValue[key]
-    }
-  },
+  filters: {},
   data() {
     return {
-      list: [],
-      total: 0,
-      listLoading: true,
-      listQuery: {
-        name: '',
-        start_time: '',
-        end_time: '',
-        type: 2,
-        page: 1,
-        limit: 20
-      },
-      userGroup: []
+      detail: [],
+      listLoading: true
     }
   },
   created() {
@@ -147,15 +114,10 @@ export default {
   methods: {
     getList() {
       this.listLoading = true
-      getChangeOrderList(this.listQuery).then(response => {
-        this.list = response.data
-        this.total = response.total
+      getInfo().then(response => {
+        this.detail = response
         this.listLoading = false
       })
-    },
-    handleFilter() {
-      this.listQuery.page = 1
-      this.getList()
     }
   }
 }
